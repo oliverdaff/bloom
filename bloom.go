7ac6a6f05b48c235df6e4c2b4ece5602da478863
Oliver Daff <oliverdaff@users.noreply.github.com>
2020-09-23 13:42:59 +1000

    added hash functions

--- bloom.go
+++ bloom.go
@@ -1,7 +1,10 @@
 package bloom
 
 import (
+	"hash/fnv"
 	"math"
+
+	"github.com/spaolacci/murmur3"
 )
 
 //
@@ -20,4 +23,27 @@ func writeBit(bitsArray []byte, index uint) []byte {
 	element, bit := findBitCoords(index)
 	bitsArray[element] = bitsArray[element] | (1 << bit)
 	return bitsArray
+}
+
+func key2Position(hashFunctions []func(uint32, uint32) uint32,
+	seed uint32, key []byte) []uint32 {
+	hM := murmur3.Sum32WithSeed(key, seed)
+	hasherV := fnv.New32()
+	hasherV.Write(key)
+	hV := hasherV.Sum32()
+	positions := make([]uint32, len(hashFunctions))
+	for i, h := range hashFunctions {
+		positions[i] = h(hM, hV)
+	}
+	return positions
+}
+
+func initHashFunctions(numHashFunctions uint32, numBits uint32) []func(uint32, uint32) uint32 {
+	hashFunctions := make([]func(uint32, uint32) uint32, numHashFunctions)
+	for i := uint32(0); i < numHashFunctions; i++ {
+		hashFunctions[i] = func(h1 uint32, h2 uint32) uint32 {
+			return (h1 + i*h2 + i*i) % numBits
+		}
+	}
+	return hashFunctions
 }
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/oliverdaff/bloom
+
+go 1.15
+
+require github.com/spaolacci/murmur3 v1.1.0
